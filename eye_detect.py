700a9853bc20d9e01fc58695776e676a81f054fc
yan860201 <yan860201@gmail.com>
2023-11-24 14:24:06 +0800

    新增註解，並改成雙眼偵測

--- eye_detect.py
+++ eye_detect.py
@@ -1,39 +1,46 @@
 import cv2
 import numpy as np
-import dlib
 
+# 輸入來源`,這邊使用示範影片`
 cap = cv2.VideoCapture("eye_recording.flv")
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-def midpoint(p1 ,p2):
-    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+def midpoint(p1, p2):
+    return int((p1 + p2) / 2)
+
 
 while True:
-    _, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, frame = cap.read()
 
-    faces = detector(gray)
-    for face in faces:
-        #x, y = face.left(), face.top()
-        #x1, y1 = face.right(), face.bottom()
-        #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+    if ret is False:  # 如果沒輸入訊號就中斷
+        break
 
-        landmarks = predictor(gray, face)
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
-        center_top = midpoint(landmarks.part(37), landmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 轉成灰階
+    gray_frame = cv2.GaussianBlur(gray_frame, (7, 7), 0)  # 高斯模糊消除雜訊
 
-        hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
-        ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+    _, threshold = cv2.threshold(
+        gray_frame, 5, 255, cv2.THRESH_BINARY_INV
+    )  # 影像二值化，將顏色較深的區域做區隔
 
-    cv2.imshow("Frame", frame)
+    threshold = cv2.dilate(threshold, np.ones((7, 7)))  # 擴張
+    # cv2.imshow("擴張", threshold)
+    threshold = cv2.erode(threshold, np.ones((8, 8)))  # 侵蝕，此兩步驟平滑化二值化影像的邊緣
+    # cv2.imshow("侵蝕", threshold)
+    contours, _ = cv2.findContours(
+        threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )  # 將邊緣取出
 
-    key = cv2.waitKey(1)
+    # 將取出的邊緣依範圍大小做排序，因為可能會取到一些雜訊的邊緣，我們只要最大的那個範圍，也就是瞳孔的邊緣
+    contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+    for cnt in contours:
+        # (x, y, w, h) = cv2.boundingRect(cnt)
+        cv2.drawContours(frame, [cnt], -1, (0, 0, 255), 2)  # 將範圍畫出來
+        break  # 只取最大的範圍
+
+    cv2.imshow("frame", frame)
+    key = cv2.waitKey(30)
+    # 按Esc可結束
     if key == 27:
         break
 
-cap.release()
 cv2.destroyAllWindows()
--- eye_motion_tracking.py
+++ eye_motion_tracking.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 import dlib
 
-cap = cv2.VideoCapture(0)
+# 輸入來源，輸入攝影機訊號
+cap = cv2.VideoCapture(1)
 
+# 臉部節點預測器
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
@@ -14,68 +16,80 @@ def midpoint(p1, p2):
 
 while True:
     ret, frame = cap.read()
-    if ret is False:
+
+    if ret is False:  # 如果沒輸入訊號就中斷
         break
 
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # 於影像中識別人臉
+    faces = detector(frame)
 
-    faces = detector(gray_frame)
-
-    # print(len(faces))
-
+    # 若有識別到人臉
     if faces:
         for face in faces:
-            landmarks = predictor(gray_frame, face)
+            # 預測識別到的臉部節點
+            landmarks = predictor(frame, face)
+
+            # 從影像中用預測的節點擷取右眼的範圍
             roi = frame[
                 int((landmarks.part(19).y + landmarks.part(37).y) / 2) : landmarks.part(
                     30
                 ).y
                 - 30,
-                landmarks.part(17).x : landmarks.part(27).x,
+                landmarks.part(17).x : landmarks.part(26).x,
             ]
             rows, cols, _ = roi.shape
-            gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-            gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
+            gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)  # 轉成灰階
+            gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)  # 高斯模糊消除雜訊
 
-            _, threshold = cv2.threshold(gray_roi, 25, 255, cv2.THRESH_BINARY_INV)
-            threshold = cv2.dilate(threshold, np.ones((7, 7)))
-            threshold = cv2.erode(threshold, np.ones((8, 8)))
+            _, threshold = cv2.threshold(
+                gray_roi, 20, 255, cv2.THRESH_BINARY_INV
+            )  # 影像二值化，將顏色較深的區域做區隔***第二個參數為二值化分界，需視情況調整***
+            threshold = cv2.dilate(threshold, np.ones((7, 7)))  # 擴張
+            threshold = cv2.erode(threshold, np.ones((8, 8)))  # 侵蝕，此兩步驟平滑化二值化影像的邊緣
             contours, _ = cv2.findContours(
                 threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-            )
+            )  # 將邊緣取出
+
+            # 將取出的邊緣依範圍大小做排序，因為可能會取到一些雜訊的邊緣，我們只要最大的那個範圍，也就是瞳孔的邊緣
             contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-
+            i = 0
             for cnt in contours:
-                (x, y, w, h) = cv2.boundingRect(cnt)
+                cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 2)  # 將範圍畫出來
 
-                # cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
-                cv2.rectangle(
-                    roi,
-                    (x + int(w / 4), y + int(h / 4)),
-                    (x + int(3 * w / 4), y + int(3 * h / 4)),
-                    (255, 0, 0),
-                    2,
-                )
-                cv2.line(
-                    roi, (x + int(w / 2), 0), (x + int(w / 2), rows), (0, 255, 0), 2
-                )
-                cv2.line(
-                    roi, (0, y + int(h / 2)), (cols, y + int(h / 2)), (0, 255, 0), 2
-                )
-                break
+                # 以下為取出邊緣的bounding box並標出十字
+                # (x, y, w, h) = cv2.boundingRect(cnt)
+                # cv2.rectangle(
+                #     roi,
+                #     (x + int(w / 4), y + int(h / 4)),
+                #     (x + int(3 * w / 4), y + int(3 * h / 4)),
+                #     (255, 0, 0),
+                #     2,
+                # )
+                # cv2.line(
+                #     roi, (x + int(w / 2), 0), (x + int(w / 2), rows), (0, 255, 0), 2
+                # )
+                # cv2.line(
+                #     roi, (0, y + int(h / 2)), (cols, y + int(h / 2)), (0, 255, 0), 2
+                # )
+                if i == 1:
+                    break  # 取完左右兩眼就結束
+                i += 1
+            # 將處理好的的影像再放回原始影像中
             frame[
                 int((landmarks.part(19).y + landmarks.part(37).y) / 2) : landmarks.part(
                     30
                 ).y
                 - 30,
-                landmarks.part(17).x : landmarks.part(27).x,
+                landmarks.part(17).x : landmarks.part(26).x,
             ] = roi
             roi = frame
+            break
     else:
         roi = frame
 
     cv2.imshow("Roi", roi)
     key = cv2.waitKey(30)
+    # 按Esc可結束
     if key == 27:
         break
 
